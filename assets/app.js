3eca372244c276a4aee8176a566c80d97ad1a94c
jenmaz12 <jenmaz12@gmail.com>
2019-02-12 00:05:34 -0500

    burger added

--- assets/app.js
+++ assets/app.js
@@ -2,8 +2,8 @@ window.onload = function() {
     function mobile_layout(){
         var viewportWidth = $(window).width();
         if (viewportWidth < 375) {
-            $(".contact-row").addClass("justify-content-sm-center");
-            $(".contact-column").addClass("justify-content-sm-center");
+            $(".contact-row").addClass("justify-content-center");
+            $(".contact-column").addClass("justify-content-center");
             $(".contact-image").addClass("row");
         }
     }
